3d9dcfcbace4faf93e0837d0b1de85baa0685153
johannes <johannes.augustin@t-systems.de>
2020-02-26 14:08:57 +0100

    Define Raspberry GPIO17 as reset pin Increase delays for better reliability

--- reset_lgw.sh
+++ reset_lgw.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-# This script is intended to be used on IoT Starter Kit platform, it performs
+# This script is intended to be used with RAK2245
 # the following actions:
-#       - export/unpexort GPIO7 used to reset the SX1301 chip
+#       - export/unpexort GPI17 used to reset the SX1301 chip
 #
 # Usage examples:
 #       ./reset_lgw.sh stop
 #       ./reset_lgw.sh start
 
-# The reset pin of SX1301 is wired with RPi GPIO7
+# The reset pin of SX1301 is wired with RPi GPI17
 # If used on another platform, the GPIO number can be given as parameter.
 if [ -z "$2" ]; then 
-    IOT_SK_SX1301_RESET_PIN=7
+    IOT_SK_SX1301_RESET_PIN=17
 else
     IOT_SK_SX1301_RESET_PIN=$2
 fi
@@ -19,26 +19,26 @@ fi
 echo "Accessing concentrator reset pin through GPIO$IOT_SK_SX1301_RESET_PIN..."
 
 WAIT_GPIO() {
-    sleep 0.1
+    sleep 0.5
 }
 
 iot_sk_init() {
-    # setup GPIO 7
+    # setup GPIO
     echo "$IOT_SK_SX1301_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
 
-    # set GPIO 7 as output
+    # set GPIO as output
     echo "out" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/direction; WAIT_GPIO
 
     # write output for SX1301 reset
     echo "1" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/value; WAIT_GPIO
     echo "0" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/value; WAIT_GPIO
 
-    # set GPIO 7 as input
+    # set GPIO as input
     echo "in" > /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN/direction; WAIT_GPIO
 }
 
 iot_sk_term() {
-    # cleanup GPIO 7
+    # cleanup GPIO
     if [ -d /sys/class/gpio/gpio$IOT_SK_SX1301_RESET_PIN ]
     then
         echo "$IOT_SK_SX1301_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
